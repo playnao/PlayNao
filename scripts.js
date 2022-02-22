44650581a4496f7b53fa60f4d9ea5204a4aceba2
playnao <100192528+playnao@users.noreply.github.com>
2022-02-22 10:59:17 +0100

    PlayNao

---
+++ scripts.js
@@ -0,0 +1,40 @@
+/* ! Cambio scritte */
+
+function scrittaNao() {
+    var x = document.getElementById("g-name");
+    x.innerText = "Gruppo programmazione NAO:";
+    var y = document.getElementById("g-text");
+    y.innerText = "Il lavoro più arduo e difficile è spettato al gruppo della programmazione del Nao infatti si sono dovuti impegnare nel capire come utilizzare un robot facendo tesoro della loro inventiva è stato anche il lavoro più lungo in termini di tempo per diverse problematiche legate per esempio alla  connessione, o al fatto che è una macchina delicata e costosa,  per questo i membri scelti sono state le persone più competenti possibili si sono tutti impegnati al massimo per questo progetto, e lavorando hanno imparato moltissime nozioni utili per il mondo della robotica un settore che si sta sviluppando e inizierà pian piano a entrare nella nostra vita quotidiana.";
+    var z = document.getElementById("g-title");
+    z.classList.add("display-none");
+  }
+
+
+  function scrittaSocial() {
+    var x = document.getElementById("g-name");
+    x.innerHTML = "Gruppo social:";
+    var y = document.getElementById("g-text");
+    y.innerText = "Il gruppo Social è stato molto produttivo si sono efficentemente occupati della creazione di account Facebook Instagram e Tiktok per la divulgazione delle informazioni circa il nostro progetto, per spargere la nostra voce, per farci sentire con l'ambizioso progetto di essere l'account Nao più seguito al mondo.";
+    var z = document.getElementById("g-title");
+    z.classList.add("display-none");
+}
+
+
+  function scrittaLogo() {
+    var x = document.getElementById("g-name");
+    x.innerHTML = "Gruppo design:";
+    var y = document.getElementById("g-text");
+    y.innerText = "Può sembrare un lavoro banale ma così non è stato, la prima impressione è la più importante, sono riusciti ad occuparsi del logo e del nome, hanno adattato le loro doti artistiche per riuscire ad elaborare un buon prodotto da usufruire per la nostra pagina PlayNao per dare un ottimo aspetto estetico ";
+    var z = document.getElementById("g-title");
+    z.classList.add("display-none");
+}
+
+
+  function scrittaSito() {
+    var x = document.getElementById("g-name");
+    x.innerHTML = "Gruppo creazione sito web:";
+    var y = document.getElementById("g-text");
+    y.innerText = "Il gruppo della programmazione è stato davvero straordinario si è infatti dilettato nella creazione del sito web da cui state leggendo questo articolo. Il loro compito consisteva nel creare un sito utilizzando il linguaggio HTML, CSS e JavaScript. Siamo partiti da zero e sono riusciti a realizzare il loro ambizioso progetto con tanto lavoro e impegno nonché dedizione, nonostante le difficoltà niente li ha fermati! Il loro impegno è stato costante e mai assente hanno creato il volto della nostra squadra, è stato un lavoro difficile ma sono riusciti grazie alle loro abilità a definire il sito internet.";
+    var z = document.getElementById("g-title");
+    z.classList.add("display-none");
+}
