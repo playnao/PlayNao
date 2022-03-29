230c942b240cdd18c5bbf2913ce0960d3f7db475
playnao <100192528+playnao@users.noreply.github.com>
2022-03-29 11:08:35 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ function scrittaNao() {
     var x = document.getElementById("g-name");
     x.innerText = "Gruppo NAO:";
     var y = document.getElementById("g-text");
-    y.innerText = "Il gruppo nao si è occupato del lavoro a cui serviva maggiore concentrazione e serietà, infatti si sono dovuti impegnare nel capire come utilizzare un robot facendo tesoro della loro creatività. È stato anche il lavoro più lungo in termini di tempo per diverse problematiche riconducibili alla  connessione,delicatezza e ristretta possibilità di movimenti del robot, per questo i membri scelti sono state le persone più professionali. Lavorando insieme hanno imparato moltissime nozioni utili riguardo il mondo della robotica un settore che si sta sviluppando e inizierà pian piano a entrare nella nostra vita quotidiana";
+    y.innerText = "Il gruppo nao si è occupato del lavoro a cui serviva maggiore concentrazione e serietà, infatti si sono dovuti impegnare nel capire come utilizzare un robot facendo tesoro della loro creatività. È stato anche il lavoro più lungo in termini di tempo per diverse problematiche riconducibili alla connessione, delicatezza e ristretta possibilità di movimenti del robot, per questo i membri scelti sono state le persone più professionali. Lavorando insieme hanno imparato moltissime nozioni utili riguardo il mondo della robotica un settore che si sta sviluppando e inizierà pian piano a entrare nella nostra vita quotidiana";
     var z = document.getElementById("g-title");
     z.classList.add("display-none");
   }
@@ -14,7 +14,7 @@ function scrittaNao() {
     var x = document.getElementById("g-name");
     x.innerHTML = "Gruppo social:";
     var y = document.getElementById("g-text");
-    y.innerText = "Lo scopo del gruppo social è stato occuparsi della creazione e della gestione degli account Facebook, Instagram e Tiktok per la divulgazione delle informazioni riguardanti il nostro progetto e a tutto ciò che abbiamo fatto e stiamo facendo durante queste settimane di lavoro. Se vuoi dare un'occhiata ai nostri lavori sui social, puoi trovarli in fondo alla pagina!";
+    y.innerText = "Lo scopo del gruppo social era la creazione e la gestione degli account Facebook, Instagram e Tiktok per la divulgazione delle informazioni riguardanti il nostro progetto e a tutto ciò che abbiamo fatto e stiamo facendo durante queste settimane di lavoro. Se vuoi dare un'occhiata ai nostri lavori sui social, puoi trovarli in fondo alla pagina!";
     var z = document.getElementById("g-title");
     z.classList.add("display-none");
 }
